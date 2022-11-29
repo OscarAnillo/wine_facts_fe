9e19dee709268df8d40f47a3ac69bd62e87a7425
oscar anillo <oscarani14@gmail.com>
2022-11-29 17:00:19 -0500

    add image and data facts for wine

---
+++ src/Data/wine-data.js
@@ -0,0 +1,13 @@
+export const wineFacts = {
+  redWine: {
+    image: "images/red-wine.jpg",
+    facts: [
+      "Most wines are not vegetarian or vegan-friendly.",
+      "A 750 ml bottle of wine contains about 6 glasses worth of wine.",
+      "It is best to store wine away from heat.",
+      "Oak imparts a buttery vanilla flavor to wine.",
+      "Horizontal storage is best for wine bottles.",
+      "Spitting is acceptable at wine tastings.",
+    ],
+  },
+};
